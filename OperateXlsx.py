0305dbc064712e05ccbb92dcbc77706ffdbd0f34
jiangzhexin <2685916454@qq.com>
2023-01-06 20:01:09 +0800

    new file:   OperateXlsx.py 	new file:   ToIco.py 	new file:   animedata.py

---
+++ OperateXlsx.py
@@ -0,0 +1,11 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook  # 对xlsx文件的读写
+
+
+def outxlsx(listt, title, name):  # 将列表listt输出到"{name}.xlsx"
+    wb = Workbook()
+    ws = wb.active
+    ws.append(title)
+    for i in listt:
+        ws.append(i)
+    wb.save(name+".xlsx")
---
+++ ToIco.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+
+def start(infile, savepath):
+    im = Image.open(infile)
+    width, height = im.size  # 获取图片宽和高
+    max_edge = max(width, height)  # 计算最大边长
+    canvas = Image.new('RGBA', (max_edge, max_edge), (0, 0, 0, 0))  # 创建一个透明画布
+    canvas.paste(im, ((max_edge - width) // 2,
+                      (max_edge - height) // 2))  # 将图片粘贴到画布上
+    im_512 = canvas.resize((256, 256))  # 将图片转换为256*256
+    im_512.save(savepath + '主视觉图.ico')  # 保存ico图标
