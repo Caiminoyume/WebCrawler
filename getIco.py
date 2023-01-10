f4cb1d06ce34662b1f13b1047384108eb1cfedb6
jiangzhexin <2685916454@qq.com>
2023-01-10 19:12:39 +0800

    modified:   getIco.py 	modified:   main.py
    Changes not staged for commit:
    	deleted:    OperateHtml.py
    	modified:   getanimes.py
    
    Untracked files:
    	web.py

--- getIco.py
+++ getIco.py
@@ -1,8 +1,9 @@
 from PIL import Image
-from OperateHtml import animeweb
+from web import animeweb
 
 
 def toico(infile):
+    icopath = '.\\file\\icos\\'
     im = Image.open(infile)
     width, height = im.size  # 获取图片宽和高
     max_edge = max(width, height)  # 计算最大边长
@@ -10,13 +11,13 @@ def toico(infile):
     canvas.paste(im, ((max_edge - width) // 2,
                       (max_edge - height) // 2))  # 将图片粘贴到画布上
     im_512 = canvas.resize((256, 256))  # 将图片转换为256*256
-    im_512.save('主视觉图.ico')  # 保存ico图标
+    im_512.save(icopath + '主视觉图.ico')  # 保存ico图标
 
 
-def getanime_photo(animeID):
+def saveanimeimage(animeID):
     imgpath = '.\\file\\images\\'
     anime = animeweb(animeID)
-    img = anime.getanimephoto()
+    img = anime.getanimeimage()
     with open(imgpath + str(animeID) + '.jpg', "wb") as file:
         file.write(img)
     toico(imgpath + str(animeID) + '.jpg')
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # 输入要求：（起始animeID，终止animeID）；作用：将anime的基本信息保存在animeID.xlsx中
 from getanimes import getanimesdata
 # 输入要求：（animeID）；作用，获得番的主视觉图.ico
-from getIco import getanime_photo
+from getico import saveanimeimage
+from web import animeweb
 
 
 def main():
     #getanimesdata(2882, 3000)
-    getanime_photo(2833)
+    saveanimeimage(2833)
+    # a = animeweb(2822)
+    # ah = a.getanimedata()
+    # print(ah)
+    # savehtml(ah, 'abc')
 
 
 if __name__ == '__main__':
