54806f879561992015b5ce1e05b3f3eda1380e7d
jiangzhexin <2685916454@qq.com>
2023-02-28 06:24:45 +0800

    modified:   .vscode/launch.json 	modified:   1.py 	modified:   2.py 	new file:   file/0.txt 	modified:   file/animeID.xlsx 	modified:   file/data.txt 	modified:   file/images/2833.jpg 	modified:   getimage.py 	modified:   message.py 	modified:   qbittorrent.py 	modified:   web.py

--- message.py
+++ message.py
@@ -2,10 +2,13 @@ import re
 
 
 def debug():
-    me = dataclear(
-        '[织梦字幕组][别当欧尼酱了 お兄ちゃんはおしまい][04集][1080P][AVC][繁日双语]')
-    a = me.getepisode()
-    print(a)
+    with open('./file/0.txt', 'r') as f:
+        for l in f:
+            print(l, end=' ')
+            me = dataclear(l)
+            print(me.getepisode(), end=' ')
+            print(me.zimufengzhuang(), end=' ')
+            print(me.zimuyuyan())
 
 
 class dataclear:
@@ -21,35 +24,31 @@ class dataclear:
             else:
                 return '/'
 
-    def getepisode(self):  # 取集数
-        rules = [
-            '\[[0-9]{2}\]',
-            ' [0-9]{2} ',
-            '\[[0-9]{2}集\]',
-            '\[[0-9]{2}v2\]',
-            ' [0-9]{2}v2 ',
-            '\[第[0-9]{2}话\]'
-        ]
-        return self.rerules(rules, '[0-9]{2}')
+    def getepisode(self):
+        match = re.search(r'(\[| |\[第|\[未删减)([0-9]{2})(\]| |集|v2|话)', self.txt)
+        if match:
+            return match.group(2)
 
     def zimuyuyan(self):  # 字幕语言
-        rules = [
-            '\[[简繁日英]{1,3}',
-            '\[CH[ST]\]',
-            '\[GB\]',
-            '\[BIG5\]'
-        ]
-        return self.rerules(rules)
+        sc = re.search(r'(\[| )(简|CHS|GB)', self.txt, re.I)
+        tc = re.search(r'(\[| )简?(繁|CHT|BIG5)', self.txt, re.I)
+        jp = re.search(r'(\[| )简?繁?(日)', self.txt, re.I)
+        eng = re.search(r'(\[| )简?繁?日?(英)', self.txt, re.I)
+        r = ''
+        if sc:
+            r += '简'
+        if tc:
+            r += '繁'
+        if jp:
+            r += '日'
+        if eng:
+            r += '英'
+        return r
 
     def zimufengzhuang(self):  # 字幕封装方式
-        rules = [
-            '内[封嵌]\]',
-            '内[封嵌]字幕\]',
-            '内[封嵌] ',
-            '外挂\]',
-            '外挂 ',
-            '外挂\]',
-        ]
+        match = re.search(r'(内封|内嵌|外挂)(\]|字幕\]| )', self.txt)
+        if match:
+            return match.group(1)
 
 
 if __name__ == "__main__":
